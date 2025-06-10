e0966668df415f13571ee70fbd590d0a240b9510
Prashant Kumar Khidhari <22btrcv006@jainuniversity.ac.in>
2025-06-10 23:22:43 +0530

    Create LinearSearchMain.java

---
+++ LinearSearchMain.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class LinearSearchMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {1, 3, 5, 7, 9, 11};
+        System.out.print("Enter element to search: ");
+        int key = sc.nextInt();
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) System.out.println("Element found at index: " + index);
+        else System.out.println("Element not found");
+        sc.close();
+    }
+}
