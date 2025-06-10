6e3242ae2c91cf0086c4cea889c893fab7bec137
Prashant Kumar Khidhari <22btrcv006@jainuniversity.ac.in>
2025-06-10 23:27:28 +0530

    Create VowelCheckerMain.java

---
+++ VowelCheckerMain.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class VowelCheckerMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+        ch = Character.toLowerCase(ch);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            System.out.println("Vowel");
+        else
+            System.out.println("Not a vowel");
+        sc.close();
+    }
+}
