801a77c87fb9c431fcddd3d7b36ca1d79b5514f5
Prashant Kumar Khidhari <22btrcv006@jainuniversity.ac.in>
2025-06-11 11:08:54 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+mport java.util.*;
+public class Factorial{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter any number: ");
+        int a = sc.nextInt();
+        
+        long factorial = 1;
+        for(int i=1; i<=a; i++){
+            factorial *= i;
+        }
+        System.out.println(factorial);
+        sc.close();
+    }
+}
